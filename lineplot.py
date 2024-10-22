81c7b7f0ff3fe63680985163e86b6d788ae3ef83
LucianC2025 <lucian.cruz2025@gbwl.org>
2024-10-22 19:12:40 +0000

    graphs

--- lineplot.py
+++ lineplot.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # Set Style for Charts
 plt.style.use('seaborn-v0_8-pastel')
 # print(plt.style.available) # Get the available color schemes
+
+#  ***************** CHART 1 ***************** 
 
 # Create a sample set of data
 # Remember that X is for the independent variable
@@ -21,6 +24,11 @@ plt.plot(x_vals, np.cos(x_vals))
 # Save Figure as PNG in current directory (The folder that we are in) 
 plt.savefig('lineplot.png') # x-values are from 0 to 10, y-values are from -1 to 1
 
+# Close old plot before making new one
+plt.close()
+
+
+#  ***************** CHART 2 ***************** 
 
 # Alternatively, set up a figure OBJECT
 fig = plt.figure() # NOTE: figure is a container that contains all the objects representing axes, graphics, text, and labels
@@ -35,6 +43,30 @@ ax.plot(x_vals, np.tan(x_vals), color = '#e9ff70')
 
 # You can use shortcut to define color & style
 ax.plot(x_vals, 1.5*(x_vals), '-m')
-
 fig.savefig('lineplot2.png')
+plt.close() # clear the plot before making another
+
+
+#  ***************** CHART 3 ***************** 
+
+# Demo on custimization
+plt.plot(x_vals, np.sin(x_vals), '-.m', label="sin(x)")
+plt.xlim(-1,11)
+plt.ylim(-1.5, 1.5)
+# Set axis to tight to fit your plot better
+plt.axis('tight') # zooms in, makes sure the axis aren't longer than neccessary and added ticks, the entire set of data gets plotted in a tighter box. less white space
+# other options: 'equal' OR 'image' OR etc
+
+# Add TITLES / LABELS
+plt.title('A Sine Wave')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+# Add LEGEND
+plt.legend()
+
+plt.savefig('lineplot3.png')
+plt.close()
+
+
+#  ***************** CHART 4 ***************** 
 
